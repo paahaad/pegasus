d36b71f9db22eab8336a1148632ae7ce3f3f71b1
Parvat Raj <parvat.raj2@gmail.com>
2024-10-02 15:49:22 +0530

    init: theme setup

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,13 @@
 import type { Config } from "tailwindcss";
+import { nextui } from "@nextui-org/react";
+
 
 const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
@@ -14,6 +17,19 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
+  darkMode: "class",
+  plugins: [nextui({
+    prefix: "nextui", // prefix for themes variables
+    addCommonColors: false, // override common colors (e.g. "blue", "green", "pink").
+    defaultTheme: "dark", // default theme from the themes object
+    defaultExtendTheme: "dark", // default theme to extend on custom themes
+    layout: {}, // common layout tokens (applied to all themes)
+    themes: {
+      dark: {
+        layout: {}, // dark theme layout tokens
+        colors: {}, // dark theme colors
+      },
+    },
+  }),],
 };
 export default config;
